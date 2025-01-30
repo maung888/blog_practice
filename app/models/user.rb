ede1340c14cc3808fc09706d7a19a2accbde3e17
maung888 <123016436+maung888@users.noreply.github.com>
2025-01-30 12:20:07 -0600

    details of what changes you are committing

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
 #  has_many :posts
 #  has_many :comments
 #add another comment. Yeah
+validates :first_name, presence: true
+  validates :last_name, presence: true
 
 
 end
